7f471831cd4b7e71ae500fa887ae778605d3c24d
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-02-25 23:13:34 +0700

    add load migration and model

---
+++ app/Models/Loan.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Loan extends Model
+{
+    protected $guarded = [];
+
+
+    protected $cast = [
+        'loan_date' => 'date',
+        'due_date' => 'date',
+    ];
+}
