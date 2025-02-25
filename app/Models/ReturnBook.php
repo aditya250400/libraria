05d29dcc6ece9f13e6bb3cec50ed4336b1485468
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-02-25 23:23:24 +0700

    add casting in Return Book Model

--- app/Models/ReturnBook.php
+++ app/Models/ReturnBook.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use App\Enums\ReturnBookStatus;
 use Illuminate\Database\Eloquent\Model;
 
 class ReturnBook extends Model
 {
     protected $guarded = [];
+
+
+    protected $cast = [
+        'status' => ReturnBookStatus::class,
+        'return_date' => 'date',
+    ];
 }
