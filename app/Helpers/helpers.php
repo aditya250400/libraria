e1b1b94709045c817646bb2df152d0309e662b82
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-02-27 20:59:25 +0700

    add user generator helper

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Models\User;
+
 if (!function_exists('flashMessage')) {
 
     function flashMessage($message, $type = 'success')
     {
         session()->flash('message', $message);
         session()->flash('type', $type);
+    }
+}
+
+if (!function_exists('usernameGenerator')) {
+    function usernameGenerator($name)
+    {
+        $username = strtolower(preg_replace('/\s+/', '_', trim($name)));
+
+        $original_username = $username;
+
+        $count = 1;
+
+        while (User::where('username', $username)->exists()) {
+            $username = $original_username . $count;
+            $count++;
+        }
+
+        return $username;
     }
 }
