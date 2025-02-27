df15c1abb0108a5849d0c52b929aa421f0c2b314
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-02-27 23:02:44 +0700

    add FINEPAYMENSTATUS object

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,6 +1,12 @@
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
+
+export const FINEPAYMENTSTATUS = {
+    PENDING: "TERTUNDA",
+    SUCCESS: "Sukses",
+    FAILED: "Gagal",
+};
