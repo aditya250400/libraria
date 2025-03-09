62d0414af603cd99201446b0e82380ffc8ef6e3a
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-03-09 23:14:13 +0700

    add persistent layout

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import AppLayout from '@/Layouts/AppLayout';
 
 export default function Dashboard() {
-    return <AppLayout title="Dashboard">Ini Dashboard</AppLayout>;
+    return <div>Ini Dashboard</div>;
 }
+
+Dashboard.layout = (page) => <AppLayout children={page} title="Dashboard" />;
