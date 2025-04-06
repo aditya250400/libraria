29e2f65f67aa98e922355cfbf45c6bdf923debb7
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-04-06 22:37:28 +0700

    add route, controller, resource, scope filter, scope sorting, and request validation for Loan

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
@@ -28,5 +29,25 @@ class Loan extends Model
     public function returnBook()
     {
         return $this->hasOne(ReturnBook::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->whereAny([
+                    'loan_code',
+                    'loan_date',
+                    'due_date'
+                ], 'REGEXP', $search);
+            });
+        });
+    }
+
+    public function scopeSorting(Builder $query, array $sorts)
+    {
+        $query->when($sorts['field'] ?? null && $sorts['direction'] ?? null, function ($query) use ($sorts) {
+            $query->orderBy($sorts['field'], $sorts['direction']);
+        });
     }
 }
