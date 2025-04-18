216d701547c0901cde49334237352a343a543983
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-04-18 21:00:51 +0700

    add delete roles data

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -102,4 +102,19 @@ class RoleController extends Controller
             // return to_route('admin.roles.index');
         }
     }
+
+    public function destroy(Role $role)
+    {
+        try {
+
+            $role->delete();
+
+            flashMessage(MessageType::DELETED->message('Peran'));
+
+            return to_route('admin.roles.index');
+        } catch (Throwable $e) {
+            flashMessage(MessageType::ERROR->message(error: $e->getMessage()), 'error');
+            return to_route('admin.roles.index');
+        }
+    }
 }
