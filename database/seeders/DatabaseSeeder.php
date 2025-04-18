ed8c28c53202f3c110e547f7f18c4f7ae2c754db
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-04-18 16:49:46 +0700

    add spatie permission pacakge and add role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+
         User::factory()->create([
-            'name' => 'Monkey D. Luffy',
-            'username' => 'Luffy',
+            'name' => $name = 'Luffytaro',
+            'username' => usernameGenerator($name),
             'email' => 'luffy@gmail.com'
-        ]);
+        ])->assignRole(Role::create(['name' => 'admin']));
+
+        User::factory()->create([
+            'name' => $name = 'Zorojuro',
+            'username' => usernameGenerator($name),
+            'email' => 'zoro@gmail.com'
+        ])->assignRole(Role::create(['name' => 'operator']));
+
+        User::factory()->create([
+            'name' => $name = 'Onami',
+            'username' => usernameGenerator($name),
+            'email' => 'nami@gmail.com'
+        ])->assignRole(Role::create(['name' => 'member']));
+
+
         $this->call(CategorySeeder::class);
         $this->call(PublisherSeeder::class);
     }
