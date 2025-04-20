0357ac53bdf612a46951476771209d391291dced
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-04-20 20:13:03 +0700

    show route access data

--- app/Http/Controllers/Admin/RouteAccessController.php
+++ app/Http/Controllers/Admin/RouteAccessController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\RouteAccessResource;
+use App\Models\RouteAccess;
 use Illuminate\Http\Request;
 
 class RouteAccessController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $route_accesses = RouteAccess::query()
+            ->select(['id', 'route_name', 'role_id', 'permission_id', 'created_at'])
+            ->filter(request()->only(['search']))
+            ->sorting(request()->only(['field', 'direction']))
+            ->with(['role', 'permission'])
+            ->paginate(request()->load ?? 10)
+            ->withQueryString();
+
+        return inertia('Admin/RouteAccesses/Index', [
+            'page_setting' => [
+                'title' => 'Akses Rute',
+                'subtitle' => 'Menampilkan semua data akses rute pada platform',
+            ],
+
+            'route_accesses' => RouteAccessResource::collection($route_accesses)->additional([
+                'meta' => [
+                    'has_pages' => $route_accesses->hasPages(),
+                ]
+            ]),
+            'state' => [
+                'page' => request()->page ?? 1,
+                'search' => request()->search ?? '',
+                'load' => 10,
+            ],
+        ]);
+    }
 }
--- app/Models/RouteAccess.php
+++ app/Models/RouteAccess.php
@@ -24,7 +24,7 @@ class RouteAccess extends Model
 
     public function scopeFilter(Builder $query, array $filters)
     {
-        $query->when($filters['search'], function ($query, $search) {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
             $query->where('route_name', 'REGEXP', $search)
                 ->orWhereHas('role', fn($query) => $query->where('name', 'REGEXP', $search))
                 ->orWhereHas('permission', fn($query) => $query->where('name', 'REGEXP', $search));
