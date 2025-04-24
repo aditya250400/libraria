0ddea7e07978db5f0d839fb1624632fae9bdaa90
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-04-24 22:50:37 +0700

    slicing dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,11 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return inertia('Dashboard');
+        return inertia('Dashboard', [
+            'page_setting' => [
+                'title' => 'Dashboard',
+                'subtitle' => 'Menampilkan semua statistik pada platform ini',
+            ]
+        ]);
     }
 }
